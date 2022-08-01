038e78a52aa9b14f47a0f44e63cc3486d37ff4b8
raphaeltorsu <98876200+raphaeltorsu@users.noreply.github.com>
2022-08-01 08:20:22 +0000

    Added solution to LRUCache with Leetcode

--- randoms/randomss.js
+++ randoms/randomss.js
@@ -1,4 +1,14 @@
-let date = new Date(2022, 6, 1)
-let date2 = new Date(2022, 6, 1)
-const currentMonth = date.getMonth()
-const currentYear = date.getFullYear()
+let date = new Date(2022, 8, 1)
+let date2 = new Date(2022, 8, 1)
+
+const currentDate = new Date(2022, 8, 1)
+const currentMonth = currentDate.getMonth()
+const nextMonth = currentMonth + 1
+const currentYear = currentDate.getFullYear()
+let startDateVal = new Date(currentYear, currentMonth)
+let endDateVal = new Date(currentYear, nextMonth)
+
+let currentEndDate = endDateVal.getDate()
+currentEndDate = currentEndDate - 1
+endDateVal.setDate(currentEndDate)
+console.log(startDateVal, endDateVal)
