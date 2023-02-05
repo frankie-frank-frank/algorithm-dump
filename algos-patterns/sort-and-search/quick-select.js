b2e2a51cc5f5f9228f0aeeff67f03607c0431193
raphaeltorsu <98876200+raphaeltorsu@users.noreply.github.com>
2023-02-05 10:02:34 +0000

    Complete max or min element

--- algos-patterns/sort-and-search/quick-select.js
+++ algos-patterns/sort-and-search/quick-select.js
@@ -1,26 +1,39 @@
-/**
- * @param {number[]} inputVal
- * @param {number} kthIndex
- * @param {string} type
- */
-function QuickSelect(inputVal, kthIndex, type){
-    const expectedKthIndex = type === 'largest' ? inputVal.length - kthIndex : kthIndex - 1
-    if(expectedKthIndex > inputVal.length || expectedKthIndex < 0) return -1
-    function RecursiveHelper(input, currIdx){
-        //4 => 2 - 1 = 1, 5 => 3 - 1 = 2
-    const pivotIdx = input[Math.ceil(input.length / 2) - 1]
-    const pivotValue = input[pivotIdx]
-    const currCorrectIdx = currIdx + pivotIdx + 1
-    
-    // NB: Follow this  order
+/*
+    QUICKSELECT FOR LARGEST ARRAY
+*/
+const findKthLargest = function(nums, k) {
+    const targetIndex = nums.length - k
+    return quickSelect(0, nums.length - 1, nums, targetIndex)
+};
 
-    //if larger, set the pivot to middle of right array and update value of pivot
-    //index to Math.ceil(inputVal.length) + incoming pivot index
-
-    //if smaller, use incoming pivot index - Math.ceil
-
-    //if currCorrectIdx is the index, sort and return that index
-    }
-    RecursiveHelper(inputVal, kthIndex)
-    
+const quickSelect = function (start, end, nums, targetIndex) {
+    let partitionIndex = getPartition(start, end, nums)
+    if (partitionIndex < targetIndex)
+        return quickSelect(partitionIndex + 1, end, nums, targetIndex)
+    else if (partitionIndex > targetIndex)
+        return quickSelect(start, partitionIndex - 1, nums, targetIndex)
+    else
+        return nums[partitionIndex]
 }
+
+const getPartition = function (start, end, nums) {
+    let i = start, j = start
+    while (j < end) {
+        //assurance that all elements to the left are smaller than element at nums[end]
+        if (nums[j] < nums[end]) {
+            swap(i, j, nums)
+            i++
+        }
+        j++
+    }
+    swap(i, end, nums)
+    return i;
+}
+
+const swap = function (a, b, nums) {
+    let temp = nums[a]
+    nums[a] = nums[b]
+    nums[b] = temp
+}
+
+console.log(findKthLargest([1,2,5,1,5,8], 2))
