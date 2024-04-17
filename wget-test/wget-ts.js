ee584de081c1d71d1157abc0983476bc9ed7c2f2
Raphael Torsu <torsu.pilgrim@gmail.com>
2024-04-17 15:42:32 +0000

    minor mods to wget-ts.js

--- wget-test/wget-ts.js
+++ wget-test/wget-ts.js
@@ -4,7 +4,6 @@ export function WgetCapture(url) {
   console.log('hey')
   const command = `wget --adjust-extension --span-hosts --convert-links --backup-converted --page-requisites -nd -P assets-22 ${url}`;
   const output = execSync(command).toString();
-  console.log('output array: ', output)
 }
 
-WgetCapture("https://google.com");
+WgetCapture("https://thejellybee.com/");
